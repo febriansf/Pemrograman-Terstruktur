a12249dfaf698789552a62512f0b77848f8c5cb5
febriansf <febriansf@student.uns.ac.id>
2021-10-07 23:42:41 +0700

    Update latihan03.py

--- Chapter_4/latihan03.py
+++ Chapter_4/latihan03.py
@@ -2,10 +2,12 @@ jarakKota = 795
 jarakPerLiter = 12
 maksTangki = 50
 
+# Hitung penggunaan bensin
 totalBensin = jarakKota / jarakPerLiter
 
-isiBensin = int(totalBensin // 50)
+# Hitung banyak isi bensin
+isiBensin = int(totalBensin // maksTangki)
 
-
+# Menampilkan Hasil
 print("Total Bensin yang diperlukan untuk sampai di kota C adalah", totalBensin, "Liter")
 print("Total isi bensin di pom adalah", isiBensin, "kali")
