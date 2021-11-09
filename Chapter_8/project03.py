d679147d92b06a802f93092589100b9206a42da0
Febrian Sabila Firdaus <febriansf@student.uns.ac.id>
2021-11-09 09:00:21 +0700

    Fix_ValueError

--- Chapter_8/project03.py
+++ Chapter_8/project03.py
@@ -1,13 +1,16 @@
-jumlah = int(input('Masukkan jumlah nama: '))
+try:
+    jumlah = int(input('Masukkan jumlah nama: '))
 
-list_nama = []
+    list_nama = []
 
-for i in range(jumlah):
-    nama = input('Masukkan nama ke-' + str(i+1) + ': ')
-    list_nama.append(nama)
+    for i in range(jumlah):
+        nama = input('Masukkan nama ke-' + str(i+1) + ': ')
+        list_nama.append(nama)
 
-list_nama.sort()
+    list_nama.sort()
 
-print('========================================')
-for nama in list_nama:
-    print(nama, '(', len(nama), 'karakter )')
+    print('========================================')
+    for nama in list_nama:
+        print(nama, '(', len(nama), 'karakter )')
+except ValueError:
+    print('INPUT TIDAK VALID!!')
