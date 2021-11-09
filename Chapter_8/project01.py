df4e9b726d021458c88ddde977f47562e3ae4fd0
Febrian Sabila Firdaus <febriansf@student.uns.ac.id>
2021-11-09 08:56:28 +0700

    Fix_ValueError

--- Chapter_8/project01.py
+++ Chapter_8/project01.py
@@ -1,9 +1,13 @@
-banyak = int(input('Berapa banyak bilangan yang ingin dimasukan? '))
-bil = []
+try:
+    banyak = int(input('Berapa banyak bilangan yang ingin dimasukan? '))
+    bil = []
 
-for i in range(banyak):
-    angka = int(input('Masukkan Angka Ke-' + str(i+1) + ': '))
-    bil = bil + [angka]
+    for i in range(banyak):
+        angka = int(input('Masukkan Angka Ke-' + str(i+1) + ': '))
+        bil = bil + [angka]
 
-bil.sort(reverse=True)
-print('Angka yang anda masukkan adalah', bil)
+    bil.sort(reverse=True)
+    print('Angka yang anda masukkan adalah', bil)
+
+except ValueError:
+    print('INPUT BUKAN ANGKA!!')
