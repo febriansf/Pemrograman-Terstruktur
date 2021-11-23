3190a45bc6b8dcbdb1f3fb6c3ec882b63b2669a4
Febrian Sabila Firdaus <febriansf@student.uns.ac.id>
2021-11-23 21:37:58 +0700

    Update

--- Chapter_8/project13.py
+++ Chapter_8/project13.py
@@ -1,36 +1,27 @@
 def cari_nilai_tertinggi(list):
-    #Mencari nilai akhir dan
-    #Memasukkannya ke dalam list_nilai_akhir
-    list_nilai_akhir = []
-    for data in list:
-        mid = data['mid']
-        uas = data['uas']
-        nilai_akhir = (mid + 2 * uas)/3
-        list_nilai_akhir += [round(nilai_akhir, 1)]
+    #Iterasi untuk mencari nilai akhir dan memasukkannya ke dictionary
+    for daftar in list:
+        nilai_akhir = ((daftar['mid'] + (2 * daftar['uas'])) / 3)
+        daftar['nilai_akhir'] = round(nilai_akhir, 2)
 
-    #Mencari nilai tertinggi dari list_nilai_akhir
-    #Kemudian cek apabila ada nilai akhir yang sama
-    #Variabel nomor_index untuk menyimpan nomor urut dari list nilaiMhs-
-    #-Pemilik nilai akhir tertinggi
-    nomor_index = []
-    i = 0
-    for nilai_akhir in list_nilai_akhir:
-        tertinggi = max(list_nilai_akhir)
-        if nilai_akhir == tertinggi:
-            nomor_index += [i]
-        i += 1
+    #stackoverflow is my friend
+    #Menggunakan perulangan for untuk
+    #-menentukan nilai akhir tertinggi"""
+    tertinggi = max([x['nilai_akhir'] for x in list])
 
-    #Menampilkan output nama dan nim dari pemilik nilai akhir tertinggi
-    for i in nomor_index:
-        print('Nama:', list[i]['nama'],
-              '(' + list[i]['nim'] + ')',
-              'dengan nilai akhir', list_nilai_akhir[i])
+    #Iterasi untuk mencetak nilai akhir tertinggi ke konsol
+    for nilai in list:
+        if nilai['nilai_akhir'] >= tertinggi:
+            print('Nama:', nilai['nama'],
+                  '(' + nilai['nim'] + ')',
+                  'dengan nilai akhir', nilai['nilai_akhir'])
 
 
+#Main Program
 nilaiMhs = [{'nim': 'A01', 'nama': 'Amir', 'mid': 50, 'uas': 80},
             {'nim': 'A02', 'nama': 'Budi', 'mid': 40, 'uas': 90},
             {'nim': 'A03', 'nama': 'Cici', 'mid': 50, 'uas': 50},
             {'nim': 'A04', 'nama': 'Dedi', 'mid': 20, 'uas': 30},
-            {'nim': 'A05', 'nama': 'Fifi', 'mid': 70, 'uas': 40}]
+            {'nim': 'A05', 'nama': 'Fifi', 'mid': 70, 'uas': 40}, ]
 
 cari_nilai_tertinggi(nilaiMhs)
