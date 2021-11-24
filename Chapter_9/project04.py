ec5c85bdc621e08cf6e1005de7144774659dd68d
Febrian Sabila Firdaus <febriansf@student.uns.ac.id>
2021-11-24 18:55:44 +0700

    Update project04.py

--- Chapter_9/project04.py
+++ Chapter_9/project04.py
@@ -7,7 +7,8 @@ def shuffleString(string, n=1):
     #Iterasi untuk memasukkan element ke dalam listString
     while True:
         #Mengacak tiap karakter pada string
-        #Fungsi random.sample akan mengembalikan nilai dengan tipe data list
+        #Fungsi random.sample akan memecah tiap karakter pada string, mengacaknya
+        # -dan mengembalikan nilai dengan tipe data list
         #Menggabungkan nilai kembalian dari random.sample menjadi string
         #-Dan menyimpannya ke dalam variabel randString
         randString = ''.join(random.sample(string, len(string)))
@@ -16,7 +17,7 @@ def shuffleString(string, n=1):
         if randString not in listString:
             listString.append(randString)
 
-        #Menghentikan Loop jika sudah mencapai n jumlah
+        #Menghentikan Loop jika sudah mencapai jumlah n
         if (len(listString) == n):
             break
 
